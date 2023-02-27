502ee89f76433906e72fb42853783bb31e5d2fcc
AvE-o <tt623457092@gmail.com>
2023-02-27 22:42:23 +0100

    feat: flask 'hello world' setup

---
+++ flask_test/app.py
@@ -0,0 +1,19 @@
+# Author by : ME!
+# Date : 2023-02-27
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/test')
+def hello_test():
+    return 'Test'
+
+if __name__ == '__main__':
+    # app.run()
+    app.run(debug=True) # debug mode on
+
+
