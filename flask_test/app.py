78f39b5822fd9b941cbdfe7f486801bac6be6339
AvE-o <tt623457092@gmail.com>
2023-03-08 22:13:14 -0500

    error: mysqldb always have trouble on MAC

--- flask_test/app.py
+++ flask_test/app.py
@@ -1,9 +1,20 @@
 # Author by : ME!
 # Date : 2023-02-27
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# here use localhost as example
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+# here my database name is 'users'
+app.config['MYSQL_DB'] = 'users'
+ 
+mysql = MySQL(app)
+
 
 @app.route('/')
 def index():
